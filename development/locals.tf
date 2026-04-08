e699938f679b7a803054ce19981a0055db68c185
Zaga00 <zahorodnykartur@gmail.com>
2026-04-08 19:17:42 +0300

    feat: creat removed/import blocks

--- development/locals.tf
+++ development/locals.tf
@@ -2,4 +2,21 @@ locals {
   mandatory_tags = {
     ManagedBy = "Terraform"
   }
+
+  env_config = {
+    dev1 = {
+      environment = "dev1"
+    }
+    dev2 = {
+      environment = "dev2"
+    }
+  }
+
+  project_name = "${var.environment}-${var.name}-${terraform.workspace}"
+
+  common_tags = merge(
+    var.tags,
+    local.mandatory_tags
+  )
+
 }
